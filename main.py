36e581e823ce3b15a15bbd4ad2480178777f4f66
Nicolas Maier <nicolas.maier2@gmail.com>
2022-05-11 17:42:16 +0200

    tweaked and cleaned code & behavior ; added command line arguments

--- main.py
+++ main.py
@@ -1,95 +1,99 @@
-import numpy as np
 import os
-import cv2
 import time
-import colorama
+import argparse
+import signal
+import cv2
 
-
+DEBUG = False
 PIXEL = '▀'
 
-def create_pixel(rgb1,rgb2, is_last=False):
-    if is_last:
-         return f'\x1b[38;2;{rgb1[0]};{rgb1[1]};{rgb1[2]}m' + PIXEL
-    else:
-        return f'\x1b[38;2;{rgb1[0]};{rgb1[1]};{rgb1[2]}m' + f'\x1b[48;2;{rgb2[0]};{rgb2[1]};{rgb2[2]}m' + PIXEL 
-def print_screen(image, rewrite=False):
-    size = os.get_terminal_size()
+interrupted = False
 
-    x = size[0] - 1
-    y = (size[1]*2) -8
-    if (image.shape[0]/x >=1 or  image.shape[1]/y>=1):
-        if image.shape[0]/x >=  image.shape[1]/y:
-            resize = x/image.shape[0]
-        else:
-            resize = y/image.shape[1]
-            
-        image = cv2.resize(image, (int(image.shape[1]*resize), int(image.shape[0]*resize)), interpolation = cv2.INTER_AREA)
-    string_image = ''
-    if rewrite:
-        old_height = image.shape[0]//2
-    for i in range(image.shape[0]//2):
+def create_pixels(rgb_up, rgb_down):
+    pixels = f'\x1b[38;2;{rgb_up[0]};{rgb_up[1]};{rgb_up[2]}m'
+    if rgb_down is not None:
+        pixels += f'\x1b[48;2;{rgb_down[0]};{rgb_down[1]};{rgb_down[2]}m'
+    pixels += PIXEL
+
+    return pixels
+
+def print_image(image, move_cursor_back):
+    term_size = os.get_terminal_size()
+
+    term_height = (term_size.lines - 1) * 2
+    term_width = term_size.columns
+
+    if image.shape[0] >= term_width or image.shape[1] >= term_height:
+        resize_factor = min(term_width / image.shape[1], term_height / image.shape[0])
+        image = cv2.resize(
+            image,
+            dsize=(int(image.shape[1] * resize_factor), int(image.shape[0] * resize_factor)),
+            interpolation = cv2.INTER_AREA
+        )
+
+    string_rows = []
+    for i in range(0, image.shape[0], 2):
         string_row = ''
-        row1 = image[i*2]
-        row2 = image[(i*2)+1]
-        for pix1, pix2 in zip(row1,row2):
-            string_row = string_row + create_pixel(pix1,pix2)
-        string_image = string_image + '\n' +string_row + '\x1b[0m'
-    if len(image) % 2:
-        string_row = [create_pixel(pix, (0,0,0), is_last=True) for pix in image[-1]]
-        string_row = ''.join(string_row)
-        string_image = string_image + '\n' +string_row + '\x1b[0m'
-        if rewrite:
-            
-            
-            old_height = old_height + 1
-    print(string_image, end='')
-    if rewrite:
-        print((old_height)*'\033[A', end ='')
-            
-def print_image(path='pizza2.png'):
-    image = cv2.imread(path, cv2.IMREAD_UNCHANGED)  
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) 
-    
-    
-    print_screen(image)
-def print_animated(path, loop = True, duration = 0.1, do_clear = False):
+        for pixel_up, pixel_down in zip(image[i], image[i + 1] if image.shape[0] > i + 1 else iter(lambda: None, 0)):
+            string_row += create_pixels(pixel_up, pixel_down)
+        string_row += '\x1b[0m'
+        string_rows.append(string_row)
+
+    if move_cursor_back:
+        print(f'\033[{(image.shape[0] + 1) // 2 - 1}A' + f'\033[{image.shape[1]}D', end='', flush=True)
+    print('\n'.join(string_rows), end='', flush=True)
+
+def print_animated(path, loop):
+    try:
+        path = int(path)
+    except ValueError:
+        if not os.path.isfile(path):
+            print('error: given path is not an existing file or camera id')
+            exit(1)
 
     cap = cv2.VideoCapture(path)
-    
-    while True:
-        old_time = time.time()
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_duration = 1 / fps
+
+    last_time = time.time()
+
+    first_frame = True
+    while not interrupted:
         ret, frame = cap.read()
+
         if not ret:
             if loop:
                 cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-                ret, frame = cap.read()
+                continue
             else:
                 break
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            print('Bye')
-            break
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        if do_clear: 
-            os.system('cls' if os.name == 'nt' else 'clear')
-        print_screen(frame, rewrite=True)
-        new_time = time.time()
-        if duration-(new_time-old_time)> 0 : time.sleep(duration-(new_time-old_time))
-        
-def print_camera(number=0, do_clear=False):
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
-    while True:
-        ret, frame = cap.read()
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
 
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        if do_clear: 
-            os.system('cls' if os.name == 'nt' else 'clear')
-        print_screen(frame, rewrite=True)
-#array_image = np.asarray( image, dtype="int32" )
-#print_image('pizza2.png')
-#print_animated('rickroll-rick.gif')
-print_camera()
+
+        print_image(frame, move_cursor_back=not first_frame)
+        first_frame = False
+        if DEBUG: print(f'=== frame {cap.get(cv2.CAP_PROP_POS_FRAMES)}/{frame_count}')
+
+        curr_time = time.time()
+        last_time += frame_duration
+        if last_time - curr_time >= 0:
+            time.sleep(last_time - curr_time)
+        else:
+            if DEBUG: print('can\'t keep up with framerate')
+
+    print()
+
+def interrupt_handler(sig, frame):
+    global interrupted
+    interrupted = True
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='plays a gif, video or shows your camera in your terminal')
+    parser.add_argument('source', type=str, help='file path or camera id')
+    parser.add_argument('-l', '--loop', action='store_true', help='repeat the gif/video indefinitely')
+    args = parser.parse_args()
+
+    old_handler = signal.signal(signal.SIGINT, interrupt_handler)
+    print_animated(args.source, loop=args.loop)
+    signal.signal(signal.SIGINT, old_handler)
